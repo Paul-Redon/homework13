3b17d1f44e12ddd776d77b74fd9574047475a502
hades <esenin322@gmail.com>
2026-09-23 21:21:31 +0300

    hw131313

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,21 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how GIGA IDE suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Author author1 = new Author("Лев", "Толстой");
+        Author author2 = new Author("Фёдор", "Достоевский");
+        Book book1 = new Book("Война и мир", author1, 1869);
+        Book book2 = new Book("Преступление и наказание", author2, 1866);
+        System.out.println("~~ Библиотечный справочник ~~");
+        System.out.println("Книга 1:");
+        System.out.println("Название: " + book1.getTitle());
+        System.out.println("Автор: " + book1.getAuthor().getFirstName() + " " + book1.getAuthor().getLastName());
+        System.out.println("Год публикации: " + book1.getPublishYear());
+        System.out.println("Книга 2:");
+        System.out.println("Название: " + book2.getTitle());
+        System.out.println("Автор: " + book2.getAuthor().getFirstName() + " " + book2.getAuthor().getLastName());
+        System.out.println("Год публикации: " + book2.getPublishYear());
+        book1.setPublishYear(1873);
+        System.out.println("~~ После изменения года публикации ~~");
+        System.out.println("Книга " + book1.getTitle() + " — новый год: " + book1.getPublishYear());
         }
     }
